a34a2c54546195ef50d1bf6fc7a4e94f6e1098d8
Maha Farghaly <mahafarghalymo@gmail.com>
2025-06-01 14:01:05 +0300

    Update to version 0.0.2
    Update example to use single file

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:app_settings/app_settings.dart';
 import 'package:biometrics_plugin/biometrics_plugin.dart';
-import 'package:biometrics_plugin_example/biometric_screen.dart';
 import 'package:flutter/material.dart';
 
 
@@ -15,10 +15,106 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: const Text('Biometric Plugin')),
         body: BiometricScreen(),
       ),
+    );
+  }
+}
+
+class BiometricScreen extends StatefulWidget {
+  const BiometricScreen({super.key});
+
+  @override
+  State<BiometricScreen> createState() => _BiometricScreenState();
+}
+
+class _BiometricScreenState extends State<BiometricScreen>
+    with WidgetsBindingObserver {
+  String _authResult = 'Waiting...';
+  final biometricAuth = BiometricAuth();
+  BiometricStatus? status;
+  bool _dialogShown = false;
+  bool _isAuthenticated = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      authenticateUser();
+    });
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      // App came back to foreground, re-check biometric status
+      authenticateUser();
+    }
+  }
+
+  Future<void> authenticateUser() async {
+    if (_isAuthenticated) return;
+    _dialogShown = false;
+    status = await biometricAuth.authenticate(
+      localizedReason: "Authenticate to use the app",
+      userCredentials: {"email": "maha@gmail.com", "password": "1234"},
+      stickyAuth: false,
+    );
+
+    print("Biometric status: $status");
+
+    setState(() {
+      _authResult = status.toString();
+    });
+    if (status == BiometricStatus.success) {
+      _isAuthenticated = true; // ✅ mark success
+    }
+    if (status == BiometricStatus.biometricNotActivated && !_dialogShown) {
+      _dialogShown = true;
+      Future.delayed(Duration(milliseconds: 200), () {
+        showDialog(context: context, builder: (context) => SettingsAlert());
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Authentication Result: $_authResult'));
+  }
+}
+class SettingsAlert extends StatelessWidget {
+  const SettingsAlert({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  AlertDialog(
+      title: Text('Biometric Status'),
+      content: Text('Biometric is not activated'),
+      actions: [
+        TextButton(
+          onPressed: () async {
+            await AppSettings.openAppSettings(type: AppSettingsType.security);
+            Navigator.of(context).pop();
+          },
+          child: Text('Open Settings'),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Dismiss'),
+        ),
+      ],
     );
   }
 }
